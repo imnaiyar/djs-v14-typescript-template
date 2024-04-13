d9a4e1e948cfff7bb776e5bec13fe594dc706018
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-04-14 05:22:18 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 export default {
-  OWNER: ["851588007697580033", "617989475078897675", "616511099659091978"],
+  OWNER: [""],
   PREFIX: ",",
-  BOT_ICON: "https://cdn.imnaiyar.site/bot-icon.gif",
-
-  Support: "https://discord.gg/u9zUjWbbQ4",
+  BOT_ICON: "",
 
   EMBED_COLORS: {
     BOT_EMBED: "#48d4ca",
